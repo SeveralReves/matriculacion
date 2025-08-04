b2717443db5592ed66747fda9edfe42e8c61e5f4
Gabo Duran <gabrielalexander2@gmail.com>
2025-08-04 01:01:32 -0400

    feat: add axios sanctum csrf

--- resources/js/utils/axiosInstance.js
+++ resources/js/utils/axiosInstance.js
@@ -1,32 +1,10 @@
 // utils/axiosInstance.js
 import axios from 'axios';
 
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return decodeURIComponent(parts.pop().split(';').shift());
-}
-
 axios.defaults.withCredentials = true;
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-const instance = axios.create({
-  baseURL: '/',
-  withCredentials: true,
-});
-
 export const fetchWithAuth = async (method, url, data = {}) => {
-  await instance.get('/sanctum/csrf-cookie'); // asegura que XSRF-TOKEN esté actualizado
-  const token = getCookie('XSRF-TOKEN');
-
-  return instance({
-    method,
-    url,
-    data,
-    headers: {
-      'X-XSRF-TOKEN': token,
-    },
-  });
+  await axios.get('/sanctum/csrf-cookie');
+  return axios({ method, url, data });
 };
-
-export default instance;
